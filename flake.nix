a61a1d25b544c65298901c0c1e8c1481bbc61e6c
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-12-02 15:14:49 -0500

    remove inputs... this just supplies a HM module

--- flake.nix
+++ flake.nix
@@ -3,14 +3,7 @@
     A nix flake that provides a home-manager module for modern unix commands.
   '';
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-22.05";
-  };
-
-  outputs = {
-    self,
-    nixpkgs,
-  } @ inputs: {
+  outputs = {self}: {
     homeManagerModule = import ./module.nix;
   };
 }
